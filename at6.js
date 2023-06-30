3b3d01e3ae26e43e0161bcaa8772274affcd06af
Caroline de Oliveira <130517427+OliveiraKroll@users.noreply.github.com>
2023-06-30 10:55:33 -0300

    Update at6.js

--- at6.js
+++ at6.js
@@ -11,3 +11,11 @@ const rl = readline.createInterface({
     output: process.stdout
 
 });
+
+rl.question("Digite a lista de produtos, separando por vírgula: ", (lista) => {
+    const itens = lista.split(',');
+    for (let i = 0; i < itens.length; i++){
+        console.log(itens[i].trim());
+    }
+    rl.close();
+});
